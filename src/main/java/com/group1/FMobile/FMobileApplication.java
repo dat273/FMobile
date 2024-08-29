d36ec1d094bc10af6ec223ec0c4c24855ef125d7
Ha Van Dat <vandat0166201@gmail.com>
2024-08-30 01:45:09 +0700

    init

---
+++ src/main/java/com/group1/FMobile/FMobileApplication.java
@@ -0,0 +1,13 @@
+package com.group1.FMobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FMobileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group1/FMobile/FMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1.FMobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
